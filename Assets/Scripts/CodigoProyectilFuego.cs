d18af502b05d4bfc6c19471b54ced1869da97cc8
ADECIRE <139186331+ADECIRE@users.noreply.github.com>
2024-11-18 16:27:14 -0600

    disparar concristal
    codigos de disparo

---
+++ Assets/Scripts/CodigoProyectilFuego.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodigoProyectilFuego : MonoBehaviour
+{
+    public float velocidad = 90f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 9f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * velocidad);
+    }
+}
