38de8d345a8f62138fe4bd81b7a5815df1380311
ADECIRE <139186331+ADECIRE@users.noreply.github.com>
2025-02-24 17:29:28 -0600

    scripts
    recarga cristal

---
+++ Assets/Scripts/ControllerCristal.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCristal : MonoBehaviour
+{
+    public GameObject Cristal;
+    public bool CristalRecolect = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void RecolectCristal()
+    {
+        if (!CristalRecolect)
+        {
+            if (Cristal != null)
+            {
+                Cristal.SetActive(false);
+                CristalRecolect = true;
+                Debug.Log("Recargada");
+                
+            }
+        }
+        else
+        {
+            Debug.Log("Cristal Recargado");
+        }
+    }
+}
---
+++ Assets/Scripts/RecargaCristal.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecargaCristal : MonoBehaviour
+{
+    private bool jugadorCerca = false;
+    private float tiempoPresionando = 0f;
+    private float tiempoRequerido = 5f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jugadorCerca)
+        {
+            if (Input.GetKey(KeyCode.R))
+            {
+                tiempoPresionando += Time.deltaTime;
+                Debug.Log("Tiempo presionando R: " + tiempoPresionando);
+
+                if (tiempoPresionando >= tiempoRequerido)
+                {
+                    Recolectcristals();
+
+                }
+            }
+            else
+            {
+                if (tiempoPresionando > 0)
+                {
+                    Debug.Log("Recarga Incompleta");
+                }
+                tiempoPresionando = 0f;
+            }
+        }
+    }
+
+    private void Recolectcristals()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        ControllerCristal ControllerCristal = player.GetComponent<ControllerCristal>();
+        if (ControllerCristal != null)
+        {
+            if (gameObject.CompareTag("Cristal"))
+            {
+                ControllerCristal.RecolectCristal();
+            }
+            
+        }
+        
+        Debug.Log("Cristal Recargado");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            jugadorCerca = true;
+            Debug.Log("Mantén presionada 'R' por 5 segundos para recargar cristal");
+        }
+    }
+
+
+}
