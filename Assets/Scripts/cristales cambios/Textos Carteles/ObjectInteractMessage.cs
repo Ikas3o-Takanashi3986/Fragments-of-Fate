e3d3fcd8d0374b1fe3c66220784bf6f1479246c4
ADECIRE <139186331+ADECIRE@users.noreply.github.com>
2025-06-02 02:59:08 -0600

    cristales
    coloca mansajes

---
+++ Assets/Scripts/cristales cambios/Textos Carteles/ObjectInteractMessage.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ObjectInteractMessage : MonoBehaviour
+{
+    public GameObject uiPromptPrefab;      
+    public GameObject infoPanelPrefab;     
+    public string objectName = "Objeto Misterioso";
+    public string actionText = "Presiona E para interactuar";
+    [TextArea] public string objectInfo = "Este objeto contiene información importante...";
+
+    private GameObject promptInstance;
+    private GameObject infoPanelInstance;
+    private bool playerInRange = false;
+    private Transform player;
+    private bool infoShown = false;
+
+    void Start()
+    {
+        
+        if (uiPromptPrefab)
+        {
+            promptInstance = Instantiate(uiPromptPrefab);
+            promptInstance.transform.SetParent(GameObject.Find("Canvas").transform, false);
+            promptInstance.SetActive(false);
+
+            
+            TextMeshProUGUI[] texts = promptInstance.GetComponentsInChildren<TextMeshProUGUI>();
+            if (texts.Length >= 2)
+            {
+                texts[0].text = objectName;
+                texts[1].text = actionText;
+            }
+        }
+
+        
+        if (infoPanelPrefab)
+        {
+            infoPanelInstance = Instantiate(infoPanelPrefab);
+            infoPanelInstance.transform.SetParent(GameObject.Find("Canvas").transform, false);
+            infoPanelInstance.SetActive(false);
+
+            TextMeshProUGUI text = infoPanelInstance.GetComponentInChildren<TextMeshProUGUI>();
+            if (text != null) text.text = objectInfo;
+        }
+    }
+
+    void Update()
+    {
+        if (playerInRange && !infoShown && Input.GetKeyDown(KeyCode.E))
+        {
+            ShowInfo();
+        }
+
+        if (infoShown && Input.GetKeyDown(KeyCode.R))
+        {
+            CloseInfo();
+        }
+
+        
+        if (promptInstance != null && playerInRange && !infoShown && player != null)
+        {
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 2f);
+            promptInstance.transform.position = screenPos;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            player = other.transform;
+            if (promptInstance != null && !infoShown)
+                promptInstance.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            player = null;
+            if (promptInstance != null)
+                promptInstance.SetActive(false);
+        }
+    }
+
+    void ShowInfo()
+    {
+        infoShown = true;
+
+        if (promptInstance) Destroy(promptInstance);
+        if (infoPanelInstance) infoPanelInstance.SetActive(true);
+
+        Time.timeScale = 0f; 
+    }
+
+    void CloseInfo()
+    {
+        if (infoPanelInstance) Destroy(infoPanelInstance);
+        Time.timeScale = 1f; 
+        Destroy(this); 
+    }
+}
--- Assets/Scripts/cristales cambios/scripts cristales/CrystalPickup.cs
+++ Assets/Scripts/cristales cambios/scripts cristales/CrystalPickup.cs
@@ -5,13 +5,70 @@ using UnityEngine;
 public class CrystalPickup : MonoBehaviour
 {
     public CrystalData crystalData;
+    public GameObject uiPromptPrefab;
 
-    private void OnTriggerEnter(Collider other)
+    private GameObject promptInstance;
+    private bool playerInRange = false;
+    private Transform player;
+
+    void Start()
     {
-        CrystalInventory inventory = other.GetComponent<CrystalInventory>();
-        if (inventory != null)
+
+        if (uiPromptPrefab)
+        {
+            promptInstance = Instantiate(uiPromptPrefab, transform.position + Vector3.up * 2f, Quaternion.identity);
+            promptInstance.transform.SetParent(GameObject.Find("Canvas").transform, false);
+            promptInstance.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            TryPickup();
+        }
+
+        if (promptInstance != null && playerInRange && player != null)
+        {
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 2f);
+            promptInstance.transform.position = screenPos;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            player = other.transform;
+
+            if (promptInstance != null)
+                promptInstance.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            player = null;
+
+            if (promptInstance != null)
+                promptInstance.SetActive(false);
+        }
+    }
+
+    void TryPickup()
+    {
+        if (player == null) return;
+
+        CrystalInventory inventory = player.GetComponent<CrystalInventory>();
+        if (inventory != null && crystalData != null)
         {
             inventory.AddCrystal(crystalData);
+            if (promptInstance != null) Destroy(promptInstance);
             Destroy(gameObject);
         }
     }
