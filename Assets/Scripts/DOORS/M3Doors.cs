3bc48da257807729018965c1714420df95a1d316
Ikas3o-Takanashi3986 <2024-009945@intecap.edu.gt>
2025-06-02 19:34:16 -0600

    agregue funcion a las 2 puertas faltantes

--- Assets/Scripts/DOORS/M3Doors.cs
+++ Assets/Scripts/DOORS/M3Doors.cs
@@ -9,8 +9,18 @@ public class M3Doors : MonoBehaviour
     public GameObject[] PuertasClosed;
     public GameObject[] PuertasOpen;
 
+    public GameObject luzParpadeo;
+
     public AudioSource audioSourceDoors;
     public AudioClip sonidoDOOR;
+
+    private Coroutine rutinaParpadeo;
+
+    private void Start()
+    {
+        if (luzParpadeo != null)
+            luzParpadeo.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -44,7 +54,27 @@ public class M3Doors : MonoBehaviour
                 audioSourceDoors.clip = sonidoDOOR;
                 audioSourceDoors.Play();
             }
+
+            if (rutinaParpadeo == null && luzParpadeo != null)
+            {
+                luzParpadeo.SetActive(true);
+                rutinaParpadeo = StartCoroutine(ParpadeoYLuzApagar());
+            }
         }
+    }
+
+    private IEnumerator ParpadeoYLuzApagar()
+    {
+        float tiempo = 0f;
+        while (tiempo < 60f)
+        {
+            luzParpadeo.SetActive(!luzParpadeo.activeSelf);
+            yield return new WaitForSeconds(0.5f);
+            tiempo += 1f;
+        }
+
+        luzParpadeo.SetActive(false);
+        rutinaParpadeo = null;
     }
 
 }
